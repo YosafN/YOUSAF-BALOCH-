09ff81cc19e263c9c7cf01e7e730da04aa2e3c2c
Lufa <84374482+xSandeep@users.noreply.github.com>
2023-09-02 20:26:49 +0530

    Update yt-playlist.js

--- commands/yt-playlist.js
+++ commands/yt-playlist.js
@@ -1,193 +1,88 @@
+const { smd, sleep, getBuffer } = require('../lib');
+const ytdl = require('ytdl-core');
+const yts = require('yt-search');
+const fs = require('fs');
 
-/**
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
-//                                                                                                      // 
-//                                         Ｖ：１．０．１                                                // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
-//                                                                                                      //
-//                                                                                                      //
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-
-CURRENTLY RUNNING ON BETA VERSION!!
-*
-   * @project_name : sᴜʜᴀɪʟ-ᴍᴅ
-   * @author : Suhail Tech Info
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo0
-   * @description : Suhail-Md ,A Multi-functional whatsapp user bot.
-   * @version 1.0.6
-*
-   * Licensed under the  GPL-3.0 License;
-* 
-   * Created By Suhail Tech Info.
-   * © 2023 Suhail-Md.
-* 
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   * SOFTWARE.
-**/
-
-
-
-
-
-
-/*const { tlang, smd, sleep, getBuffer, prefix, Config } = require('../lib')
-const ytdl = require('ytdl-secktor')
-const yts = require('secktor-pack')
-const fs = require('fs')
-var videotime = 60000 // 1000 min
-var dlsize = 250 // 250mb
-
- 
 smd({
-            pattern: "playlist",
-            desc: "Downloads video from playlist.",
-            category: "downloader",
-            filename: __filename,
-            use: '<yt playlist url>',
-        },
-        async(Void, citel, text) => {
-            const getRandom = (ext) => {  return `${Math.floor(Math.random() * 10000)}${ext}`; };
-            if (!text) {  citel.reply(`❌Please provide me a url`);   return; }
-let urlYtt = text.split('=')[1]
-console.log(urlYtt)
-var opts = { listId: urlYtt }
-yts( opts, async function ( err, playlist ) {
-	if ( err ) throw err
-  citel.reply('This Process will take a bit time.');
-  for (let i=0;i<playlist.videos.length;i++){
-    if(playlist.videos[i].videoId===undefined) continue
-    let urlYt = playlist.videos[i].videoId
+    pattern: "playlist",
+    desc: "Downloads video from playlist.",
+    category: "downloader",
+    filename: __filename,
+    use: '<yt playlist url>',
+}, async (Void, citel, text) => {
+    if (!text) {
+        citel.reply(`❌ Please provide me a YouTube playlist URL.`);
+        return;
+    }
+
     try {
-      
-                let infoYt = await ytdl.getInfo(urlYt);
-                if (infoYt.videoDetails.lengthSeconds >= videotime) continue
-                let titleYt = infoYt.videoDetails.title;
-                let randomName = getRandom(".mp4");
-                const stream = ytdl(urlYt, {  filter: (info) => info.itag == 22 || info.itag == 18, })
+        const playlist = await yts({ listId: text });
+
+        if (!playlist) {
+            citel.reply(`❌ Playlist not found.`);
+            return;
+        }
+
+        citel.reply('This process may take some time.');
+
+        for (let i = 0; i < playlist.videos.length; i++) {
+            const video = playlist.videos[i];
+
+            if (!video.url || !video.title) continue;
+
+            try {
+                const infoYt = await ytdl.getInfo(video.url);
+
+                if (infoYt.videoDetails.lengthSeconds >= videotime) continue;
+
+                const titleYt = infoYt.videoDetails.title;
+                const thumbnail = await getBuffer(infoYt.videoDetails.thumbnail.thumbnails[0].url);
+                const randomName = getRandom(".mp4");
+
+                const stream = ytdl(video.url, { filter: (info) => info.itag === '22' || info.itag === '18' })
                     .pipe(fs.createWriteStream(`./${randomName}`));
-                await new Promise((resolve, reject) => {stream.on("error", reject);stream.on("finish", resolve); });
-                let stats = fs.statSync(`./${randomName}`);
-                let fileSizeInBytes = stats.size;
-                let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-                if (fileSizeInMegabytes <= dlsize) {
-                    let search = await yts(text);
-                    let buttonMessage = {
-                        video: fs.readFileSync(`./${randomName}`),
-                        mimetype: 'video/mp4',
-                        caption: ` ⿻ Title : ${titleYt}\n ⿻ File Size : ${fileSizeInMegabytes} MB`,
-                        headerType: 4,
-                        contextInfo: {
-                            externalAdReply: {
-                                title: titleYt,
-                                body: citel.pushName,
-                                thumbnail: log0,
-                                renderLargerThumbnail: true,
-                                mediaType: 2,
-                                mediaUrl: 'https://www.youtube.com/@SuhailTechInfo0',
-                                sourceUrl: 'https://www.youtube.com/@SuhailTechInfo0'
-                            }
-                        }
-                    }
-                   Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-                } else { citel.reply(`❌ File size bigger than ${dlsize}mb.`);  }
-                fs.unlinkSync(`./${randomName}`);
-    } catch (e) {   console.log(e)   }
-            }})
 
-
-
-
- 
-            /*
-
-            const getRandom = (ext) => {
-                return `${Math.floor(Math.random() * 10000)}${ext}`;
-            };
-            if (!text) {
-                citel.reply(`❌Please provide me a url`);
-                return;
-            }
-let urlYtt = text.split('=')[1]
-console.log(urlYtt)
-var opts = { listId: urlYtt }
-yts( opts, async function ( err, playlist ) {
-	if ( err ) throw err
-  citel.reply('This Process will take a bit time.');
-  for (let i=0;i<playlist.videos.length;i++){
-    if(playlist.videos[i].videoId===undefined) continue
-    let urlYt = playlist.videos[i].videoId
-    try {
-      
-                let infoYt = await ytdl.getInfo(urlYt);
-                if (infoYt.videoDetails.lengthSeconds >= videotime) continue
-                let titleYt = infoYt.videoDetails.title;
-                let randomName = getRandom(".mp4");
-
-                const stream = ytdl(urlYt, {
-                        filter: (info) => info.itag == 22 || info.itag == 18,
-                    })
-                    .pipe(fs.createWriteStream(`./${randomName}`));
                 await new Promise((resolve, reject) => {
                     stream.on("error", reject);
                     stream.on("finish", resolve);
                 });
-                let stats = fs.statSync(`./${randomName}`);
-                let fileSizeInBytes = stats.size;
-                let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+
+                const stats = fs.statSync(`./${randomName}`);
+                const fileSizeInBytes = stats.size;
+                const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+
                 if (fileSizeInMegabytes <= dlsize) {
-                    let yts = require("secktor-pack");
-                    let search = await yts(text);
-                    let buttonMessage = {
+                    const buttonMessage = {
                         video: fs.readFileSync(`./${randomName}`),
-                        jpegThumbnail: log0,
                         mimetype: 'video/mp4',
                         fileName: `${titleYt}.mp4`,
-                        caption: ` ⿻ Title : ${titleYt}\n ⿻ File Size : ${fileSizeInMegabytes} MB`,
+                        caption: `⿻ Title: ${titleYt}\n⿻ File Size: ${fileSizeInMegabytes} MB`,
                         headerType: 4,
                         contextInfo: {
                             externalAdReply: {
                                 title: titleYt,
                                 body: citel.pushName,
-                                thumbnail: await getBuffer(search.all[0].thumbnail),
+                                thumbnail: thumbnail,
                                 renderLargerThumbnail: true,
                                 mediaType: 2,
-                                mediaUrl: search.all[0].thumbnail,
-                                sourceUrl: search.all[0].thumbnail
+                                mediaUrl: video.url,
+                                sourceUrl: video.url
                             }
                         }
                     }
-                   Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+
+                    Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
                 } else {
-                    citel.reply(`❌ File size bigger than ${dlsize}mb.`);
+                    citel.reply(`❌ File size is larger than ${dlsize}MB.`);
                 }
 
                 fs.unlinkSync(`./${randomName}`);
             } catch (e) {
-                console.log(e)
+                console.error(e);
             }
-            }})
-
-
-
-
-            
-            //
-
-
-})
-
-*/
+        }
+    } catch (error) {
+        console.error(error);
+        citel.reply(`❌ An error occurred while processing the playlist.`);
+    }
+});
