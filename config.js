ce022362040741fe6962887d1519526f75601de0
Qasim Jiho <95994827+Qasimjiho@users.noreply.github.com>
2023-09-10 22:01:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,66 +1,59 @@
-const fs = require('fs-extra')
-if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
-
-
-//═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : '923184474176';
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://qasim:qas@cluster0.mqn5gav.mongodb.net/?retryWrites=true&w=majority" ;
-global.port=5000  ; 
-global.audio = '' ; 
-global.video = '' ;
-global.blockJids = process.env.BLOCK_JID ||'120363023983262391@g.us' ;
-global.allowJids = process.env.ALLOW_JID ||'120363022922797633@g.us' ;
-global.email = 'samsamsun789@gmail.com' ;
-global.location = 'Lahore Pakistan' ;
-global.timezone  = process.env.TIME_ZONE || 'Asia/Karachi'
-global.gurl = 'https://youtube.com/c/QasimInfo' ; // add your username
-global.sudo =  process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : '923125726553'; ;
-global.devs = "923125726553"; //Dont change it From here
-global.github  = process.env.YOUR_GITHUB || 'https://github.com/QasimjihoInfo/Qasim-Md';
-global.scan  = process.env.QR_URL || 'https://replit.com/@SuhailTechInfo/Suhail-Md?v=1';
-global.website = 'https://github.com/SuhailTechInfo/Suhail-Md' ; //wa.me/+923000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://imgur.com/DrUOnZR.jpeg' ;
-module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
-  botname: process.env.BOT_NAME || 'qasim-jiho',
-  botbgm: process.env.BOT_BGM || 'false',
-  ownername:  process.env.OWNER_NAME || `Qasim jiho`,
-  author:  process.env.PACK_AUTHER || '',
-  errorChat : process.env.ERROR_CHAT|| '', // put 'chat' here to send error in chat ,where it accures
-  read_status : process.env.AUTO_READ_STATUS || 'false',
-  save_status : process.env.AUTO_SAVE_STATUS || 'false',
-  packname:  process.env.PACK_NAME || "\t   𓅋 ₊₉₂⃗⃗₃₁᩺₈ͦ₄ͪ₄ᷧ₇ͥ₄ᷞ₁⃗₇₆ 𓃮 \n\nᴋɪss ʍᴇ...💋\n\nᴄʟօsᴇ ყσυɾ ᴇყᴇs...👁️🙈\n\nʍɪss ʍᴇ...♥️\n\n\n   -ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ ᴛᴀӄᴇ 🚮 \n\n--- ρυвʟɪᴄ вσт нᴇʀᴇ ---" ,
-  autoreaction: process.env.AUTO_REACTION || 'false',
-  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokeyuntillYouPutAnWordHere',
-  alwaysonline: process.env.ALWAYS_WAPRESENCE || 'paused', // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
-  antifake :   process.env.FAKE_COUNTRY_CODE ||'923125726553',
-  readmessage: process.env.READ_MESSAGE || 'false',
-  readcmds : process.env.READ_COMMANDS || 'true',
-  HANDLERS: process.env.PREFIX || ',',
-  warncount : process.env.WARN_COUNT || 3,
-  disablepm: process.env.DISABLE_PM || "false",
-  MsgsInLog:process.env.MSGS_IN_LOG ||'false',
-  pmMsgsInLog:process.env.PM_MSGS_IN_LOGS ||'false',
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
-  antilink_values: process.env.ANTILINK_VALUES || 'https://,chat.whatsapp.com',
-  //antilinkaction: process.env.BRANCH || 'remove',
-  BRANCH: process.env.BRANCH || 'main',
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
-  caption :process.env.CAPTION || "```ᴘᴏᴡᴇʀᴇᴅ ʙʏ Qasim²²¹-jiho```",   //*『qasimjiho • qasim ᴛᴇᴄʜ』*\n youtube.com/@qasimjihoinfo0"),
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' ,
-  VERSION: process.env.VERSION || 'v.1.0.6',
-  LANG: process.env.THEME || 'QASIM',
-  menu : process.env.MENU || '', /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: QASIMJIHO-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || 'public'
-};
-
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(`Update'${__filename}'`)
-    delete require.cache[file]
-	require(file)
-})
+const fs = require('fs-extra') 
+ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' }) 
+  
+  
+ //═══════[Required Variables]════════\\ 
+ global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
+ global.port= process.env.PORT || 5000 
+ global.email = 'sam@secktor.live' 
+ global.github = 'https://github.com/nipuna15' 
+ global.location = 'Pakistan, Lahore' 
+ global.gurl = 'https://instagram.com/' // add your username 
+ global.sudo = process.env.SUDO || '923125726553' 
+ global.devs = '923125726553'; 
+ global.website = 'https://github.com/' //wa.me/+91000000000000 
+ global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' 
+ module.exports = { 
+   botname:   process.env.BOT_NAME === undefined ? '' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? '' : process.env.OWNER_NAME,  
+   author:  process.env.PACK_INFO.split(";")[0] === undefined ? '' : process.env.PACK_INFO.split(";")[0], 
+   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
+   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.PACK_INFO.split(";")[1],
+   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
+   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
+   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
+   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
+   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
+   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
+   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
+   HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
+   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
+   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
+   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE, 
+   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES, 
+   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION, 
+   BRANCH: 'main', 
+caption :process.env.CAPTION || "```ᴘᴏᴡᴇʀᴇᴅ ʙʏ Qasim²²¹-jiho```"
+   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE, 
+   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO, 
+   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY, 
+   heroku:  process.env.heroku === undefined ? false : process.env.heroku, 
+   HEROKU: { 
+     HEROKU: process.env.HEROKU ||false, 
+     API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY, 
+     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME 
+ }, 
+   VERSION: process.env.VERSION === undefined ? 'v.0.0.2' : process.env.VERSION, 
+   LANG: process.env.THEME|| '-', 
+   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE 
+ }; 
+  
+  
+ let file = require.resolve(__filename) 
+ fs.watchFile(file, () => { 
+         fs.unwatchFile(file) 
+         console.log(`Update'${__filename}'`) 
+     delete require.cache[file] 
+         require(file) 
