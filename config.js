13023ce89554719c254b4a0406332dc59905b55c
1realjj <158238355+1realjj@users.noreply.github.com>
2024-01-31 03:39:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,23 +8,23 @@ global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="samsamsun789@gmail.com"
-global.location="Lahore,Pakistan."
+global.location="Accra,Ghana."
 global.mongodb= process.env.MONGODB_URI || ""
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "120363023983262391@g.us"
 global.DATABASE_URI=process.env.DATABASE_URL || ""
 global.scan = "https://suhail-md-vtsf.onrender.com/";
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
+global.timezone= process.env.TZ || process.env.TIME_ZONE || "Accra/Ghana";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
+global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/HWPYyrfLZGG0334PUYEjuz";
+global.website=process.env.GURL || "https://chat.whatsapp.com/HWPYyrfLZGG0334PUYEjuz" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
-global.devs = "923184474176" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923184474176";
+global.devs = "233246114422" // Developer Contact
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null.233246114422";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "233246114422";
 
 
  
@@ -85,16 +85,16 @@ module.exports = {
   caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
 
  
-  author: process.env.PACK_AUTHER || "Suhail-MD",
+  author: process.env.PACK_AUTHER || "jayjey-",
   packname: process.env.PACK_NAME || "♥️",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "It'x Suhail",
+  botname : process.env.BOT_NAME  || "jay360b0t",
+  ownername:process.env.OWNER_NAME|| "It'x jayjey",
 
 
 
 
 
-  sessionName:process.env.SESSION_ID || "",  // PUT SESSION ID HERE 
+  sessionName:process.env.SESSION_ID || "SUHAIL_01_55_01_31_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiZURXSDJLaVJVYjZmQUFnMlVTRUJCY1YwNGJHMFptbVI1SzJmdjdZNnVVMD1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIjRWT295d1RYQjJENTZOcTBSOWVqbmRNd1ArcUNxTlQ2TUZWVEJydXV4RW89XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwieU5QVm5HTVA5dDZwcTJSa2ZDN3NMUEQwUmN6Q0tmVzlZcU9vdENrbS9Gbz1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcInpQS2lhWDVjMVRkZDFURkVtWE5UVmVhNGpJV0gyUEJURHlYUG9LYnNwVzQ9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwid0xxMjBmWUp4dW9mWXFkd3U1VzZBc2N6Q3lNc1lqRTd3L3BSSXNPOW9FND1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIkE1ZFdUMTk5dFhJcFI3cTd2U0pid2UxZDY5SzV2NjJ0Z3JpWDRxQm1BalE9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJ3Szh1anprRWozek5zNmpnMDY1MWhHeGRvSlF5UldHT2pBL09BT1hrWjJBPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiR2hUQm1Kb1UyN2NsNk9wT1pUNElrd1FtZE1NaDh2bUVqZ1BSQVBqQVFtUT1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJVRXM5cWRRb2FvNHBiMGp3Y3VYVmphZlFveDE3WCtPbDBUZ2JUMWlTd1ZXWXJVcUNHaGhBREQ5RXQrYUNFQjYxcG5FU1JRaml5UjFlWCtJWUVLcjJnQT09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6MTAzLFwiYWR2U2VjcmV0S2V5XCI6XCJySktBN0N1SE1KWUQydmRvUUdvWHFlMlZQSmVkclRyNEt1UmZjT3VRTFl3PVwiLFwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzXCI6W10sXCJuZXh0UHJlS2V5SWRcIjozMSxcImZpcnN0VW51cGxvYWRlZFByZUtleUlkXCI6MzEsXCJhY2NvdW50U3luY0NvdW50ZXJcIjowLFwiYWNjb3VudFNldHRpbmdzXCI6e1widW5hcmNoaXZlQ2hhdHNcIjpmYWxzZX0sXCJkZXZpY2VJZFwiOlwiYmtmdXdEeHZSdDZzQ2hmQlpVUGY4Z1wiLFwicGhvbmVJZFwiOlwiNjJjZGVmODQtNDM5MC00ZDE5LWE5NTAtYzA5MjJkYmRmZTRmXCIsXCJpZGVudGl0eUlkXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJEQ0ErcHdLWDdhdXZmTlh0djZ5SzZnMUlPbVU9XCJ9LFwicmVnaXN0ZXJlZFwiOmZhbHNlLFwiYmFja3VwVG9rZW5cIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIi9KRzdTQ3Uyb2UxL3Z5ZW9xWGlOQXhCRHk5WT1cIn0sXCJyZWdpc3RyYXRpb25cIjp7fSxcImFjY291bnRcIjp7XCJkZXRhaWxzXCI6XCJDS2o5ejhZREVQN1E1cTBHR0FFZ0FDZ0FcIixcImFjY291bnRTaWduYXR1cmVLZXlcIjpcIkVoZ1BVK09IaEgyamRoM3NHWCs4c2l2NHRmV2dGL2NyM1B4Y0lpK3JwUjQ9XCIsXCJhY2NvdW50U2lnbmF0dXJlXCI6XCIrSEt6ZFVKTEpIem9vY2lESFhlUnl0LzVRZlphTDhzektVcVlXd0d5dnluSVM3MDBqUml1REZWVWMvRndhOFh4SGJ4OEJDSFI0MGNwS0lmNFpGd0JCZz09XCIsXCJkZXZpY2VTaWduYXR1cmVcIjpcInV1MVVzYnRSNmJMS3pvUzN6WjRQV0lDTnc1T25iUVQ0U1EzdmpwcHdwV2dKM1YyUHI2VWxLODYwbnJGaEJ1SjFMMk9KaGZJUzBRK1pBWjBkSVhOZ2lRPT1cIn0sXCJtZVwiOntcImlkXCI6XCIyMzMyNDYxMTQ0MjI6MTJAcy53aGF0c2FwcC5uZXRcIixcImxpZFwiOlwiOTI3MTk4NTQ3MDI4MDI6MTJAbGlkXCJ9LFwic2lnbmFsSWRlbnRpdGllc1wiOlt7XCJpZGVudGlmaWVyXCI6e1wibmFtZVwiOlwiMjMzMjQ2MTE0NDIyOjEyQHMud2hhdHNhcHAubmV0XCIsXCJkZXZpY2VJZFwiOjB9LFwiaWRlbnRpZmllcktleVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQlJJWUQxUGpoNFI5bzNZZDdCbC92TElyK0xYMW9CZjNLOXo4WENJdnE2VWVcIn19XSxcInBsYXRmb3JtXCI6XCJpcGhvbmVcIixcImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcFwiOjE3MDY2NjYxMTN9Igp9",  // PUT SESSION ID HERE 
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API  || "false",
 
@@ -161,6 +161,6 @@ fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename
   //MsgsInLog: process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
   //readcmds:process.env.READ_COMMANDS|| "false", 
   //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
-  //read_status: process.env.AUTO_READ_STATUS || "false",
+  //read_status: process.env.AUTO_READ_STATUS || "true",
   //save_status: process.env.AUTO_SAVE_STATUS || "false",
  
