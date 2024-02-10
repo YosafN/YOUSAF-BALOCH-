9dbd0bd8934d22630e0d2fcb86fefdd8b93f1ab7
Sheriff458f5 <159548922+Sheriff458f5@users.noreply.github.com>
2024-02-11 01:38:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]
 
 //========================= [ BOT SETTINGS ] =========================\\
 global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
+global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
+global.gdbye = process.env.GOODBYE || "true"; 
+global.wlcm  = process.env.WELCOME || "true";  // Make it "false" for disable WELCOME 
 
 global.warncount = process.env.WARN_COUNT || 3,
 global.disablepm = process.env.DISABLE_PM || "false",
@@ -43,14 +43,14 @@ global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
-global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
+global.readcmds = process.env.READ_COMMAND || "true"
+global.readmessage = process.env.READ_MESSAGE || "true"
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "0741206672";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "false"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
+global.read_status = process.env.AUTO_READ_STATUS || "true"
+global.save_status = process.env.AUTO_SAVE_STATUS || "true"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
 
@@ -71,7 +71,7 @@ module.exports = {
   botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
   ownername:process.env.OWNER_NAME|| "It'x Suhail",
 
-  sessionName:process.env.SESSION_ID || "",  // PUT SESSION ID HERE 
+  sessionName:process.env.SESSION_ID || "",  //SUHAIL_11_56_02_10_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQ0MyQWNXenVicHNHdUZLREpMRjNWQks1bUZMRVRnallNL3BnMWxzZjhIOD1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImZJV2hwNHc0K3V4eWplcWRobGVYVHhvR2hNVkhhTWdFTm1TaG1VbUtBSE09XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiOERBV2ZNYVI1c3owS2hyZXZzamVmbjEvcmJDVlVIN3VYeVMxMDlIcStGND1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImlBOWY3VkZabGQ4MkxjK3pwQ1MrOXcwSVpJTnJXdnVZOXVGdWFyd243V2M9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiT0VRR3NPSXlzanFJVzhxckZsRk0zWUVSMXZYYmJpcTI4aThJT0NqYzIxVT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIlk5MlIwdDduVlc3SjJ5bVpNaGsyVWc0cTB5MnhsSmdQNmxmdFhra2ZjQ1k9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJPQVJtUmRkZFFYci9BR3R6VitVdzRjcUZneGJnT0lLeDlxV2RNNXVPUkc0PVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiUjRleTR0MTZ2enhoanpnNE95T3hsT0pQM3ExOWtmOXBGb2pzSVhKV0tpMD1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJiWFRKcnpGVS8reDZia3BrTHZUeUIzTmZUT3RBaE0vWWNhSUZVT0h5cmZPL3djanJyVytiS0xzYUkzM0lLWGZGK0VKT0FMWVhyRzByZGRYQVUzMm9qQT09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6MTMwLFwiYWR2U2VjcmV0S2V5XCI6XCJBMkkvYmVNRFRrcDdWL3p1Q0tkcW5hZU9QZzdGQTU0NitpSDdlSFF5dE5ZPVwiLFwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzXCI6W10sXCJuZXh0UHJlS2V5SWRcIjozMSxcImZpcnN0VW51cGxvYWRlZFByZUtleUlkXCI6MzEsXCJhY2NvdW50U3luY0NvdW50ZXJcIjowLFwiYWNjb3VudFNldHRpbmdzXCI6e1widW5hcmNoaXZlQ2hhdHNcIjpmYWxzZX0sXCJkZXZpY2VJZFwiOlwiMDJPa2xsTm9RMUdvRmx2dDdQeUZ3QVwiLFwicGhvbmVJZFwiOlwiNDJkZDQ3YmYtYzhhNC00ZTYxLTg0OWItYzM3Njk0YTVlODhhXCIsXCJpZGVudGl0eUlkXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCIwdlU5NUU3L1B0R1FOWjZId1RGWmxxYlJNVEU9XCJ9LFwicmVnaXN0ZXJlZFwiOmZhbHNlLFwiYmFja3VwVG9rZW5cIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIlowMTdiUXE4RjdUME9GNlhsT0N3NTRtVnNKTT1cIn0sXCJyZWdpc3RyYXRpb25cIjp7fSxcImFjY291bnRcIjp7XCJkZXRhaWxzXCI6XCJDUHozbzhBR0VNekluYTRHR0JBPVwiLFwiYWNjb3VudFNpZ25hdHVyZUtleVwiOlwiSjRKWHdvWGdzTG52cmIyUVV5dnMwMlI3VjhIMUtPYXIrTXhhUStqdXlVcz1cIixcImFjY291bnRTaWduYXR1cmVcIjpcIm0yR1pOZFYyb0R1RStHVThxZ0VvWEVWbGsydytVcEZwcEoxaEVGVkpDQ0hnTXlUc25MOWpBOUZlMlY5Mnl5TnpaUDJSd3pSZGNZMXlkRkZtYmovb0JnPT1cIixcImRldmljZVNpZ25hdHVyZVwiOlwiTUlUV3RsUjhrbnNoTVZtLytUbE5pQ2x2R3B6SVVURFF4WU51YkF2MCtpOGNzSGk4TmR5WTc1bDBobkIvcmNPRjVLSVo3R3Y3R0tDclVCQUczTWFFaWc9PVwifSxcIm1lXCI6e1wiaWRcIjpcIjI1NDc0MTIwNjY3Mjo5QHMud2hhdHNhcHAubmV0XCIsXCJsaWRcIjpcIjE5MDIwMjg2MTU5Mjc0NTo5QGxpZFwifSxcInNpZ25hbElkZW50aXRpZXNcIjpbe1wiaWRlbnRpZmllclwiOntcIm5hbWVcIjpcIjI1NDc0MTIwNjY3Mjo5QHMud2hhdHNhcHAubmV0XCIsXCJkZXZpY2VJZFwiOjB9LFwiaWRlbnRpZmllcktleVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQlNlQ1Y4S0Y0TEM1NzYyOWtGTXI3Tk5rZTFmQjlTam1xL2pNV2tQbzdzbExcIn19XSxcInBsYXRmb3JtXCI6XCJhbmRyb2lkXCIsXCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXBcIjoxNzA3NTY2MTYxfSIKfQ==  
   errorChat : process.env.ERROR_CHAT || "",
   KOYEB_API : process.env.KOYEB_API  || "false",
 
@@ -125,13 +125,13 @@ fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename
 
 // ========================= [ Disables in V.1.2.8 ] ===============================\\  
   //style : process.env.STYLE || "2",  // put '1' & "2" here to check bot styles
-  //readmessage:process.env.READ_MESSAGE|| "false",
+  //readmessage:process.env.READ_MESSAGE|| "true",
   //warncount: process.env.WARN_COUNT || 3,
   //userImages:process.env.USER_IMAGES|| "text",  // SET IMAGE AND VIDEO URL FOR BOT MENUS 
   //disablepm: process.env.DISABLE_PM || "false",
   //MsgsInLog: process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
   //readcmds:process.env.READ_COMMANDS|| "false", 
   //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
-  //read_status: process.env.AUTO_READ_STATUS || "false",
+  //read_status: process.env.AUTO_READ_STATUS || "true",
   //save_status: process.env.AUTO_SAVE_STATUS || "false",
  
