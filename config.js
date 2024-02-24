4fd76fd2a5403fd8ddcae1dabea62fc4af902bd8
Olamide-24 <158676178+Olamide-24@users.noreply.github.com>
2024-02-24 20:52:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,18 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 global.audio= "" ;  
 global.video= "" ;
 global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""                       // put your app url here,
+global.appUrl=process.env.APP_URL || "wa.me/2347044903733"                       // put your app url here,
 global.email ="olamidemhi.001@gmail.com"
 global.location="Ibadan,Nigeria."
 global.mongodb= process.env.MONGODB_URI || "mongodb+srv://olabtcminer:olabtcminer@cluster97.qutc3mq.mongodb.net/"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "120363023983262391@g.us"
-global.DATABASE_URI = process.env.DATABASE_URL || ""
+global.DATABASE_URI = process.env.DATABASE_URL || "mongodb+srv://olabtcminer:olabtcminer@cluster97.qutc3mq.mongodb.net/"
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Nigeria";
 global.github=process.env.GITHUB|| "https://github.com/Olamide-24";
-global.gurl  =process.env.GURL  ||"";
-global.website=process.env.GURL || "" ; 
+global.gurl  =process.env.GURL  ||"https://whatsapp.com/channel/0029VaSC8hfEVccITNsp9S1p";
+global.website=process.env.GURL || "https://whatsapp.com/channel/0029VaSC8hfEVccITNsp9S1p" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
 
 
@@ -31,13 +31,13 @@ global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]
 
 //========================= [ BOT SETTINGS ] =========================\\
 global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || "false"; 
+global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
+global.gdbye = process.env.GOODBYE || "true"; 
 global.wlcm  = process.env.WELCOME || "true";  // Make it "false" for disable WELCOME 
 
-global.warncount = process.env.WARN_COUNT || 3,
-global.disablepm = process.env.DISABLE_PM || "false",
-global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
+global.warncount = process.env.WARN_COUNT || 7,
+global.disablepm = process.env.DISABLE_PM || "trueg",
+global.MsgsInLog = process.env.MSGS_IN_LOG|| "true", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
 global.userImages= process.env.USER_IMAGES|| "text",
 global.waPresence= process.env.WAPRESENCE ||  "recording" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
@@ -84,8 +84,8 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
-  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "SUHAIL",
+  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "public",
+  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "🌴🌹Ølã Mhî Dhë🥷🌓",
 
 
 
@@ -118,7 +118,7 @@ module.exports = {
 
 
 
-global.isMongodb = false; 
+global.isMongodb = true; 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename}'`);delete require.cache[file];	require(file); })
  
