fd881ef1567e9050b1fcf0cc7cb15dbba57c580c
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2024-03-13 06:22:21 +0500

    config.js

--- config.js
+++ config.js
@@ -20,9 +20,9 @@ global.DATABASE_URI = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  = process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website= process.env.GURL|| "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.gurl  = process.env.GURL || "https://whatsapp.com/channel/0029VaTMSqHDeOMycCoZPF36";
+global.website= process.env.GURL|| "https://whatsapp.com/channel/0029VaTMSqHDeOMycCoZPF36" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/cde3fea280e8269ac8448.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
@@ -74,7 +74,7 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.0",
-  caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "©ʜᴜsɴᴀɪɴ-ʀᴀᴊᴘᴜᴛ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ Husnain-Rajput 』```", //*『sᴜʙsᴄʀɪʙᴇ • ᴀᴛɪғ ʟɪɴᴋs』*\n https://youtube.com/@atiflinkspk?si=YgSyJyKII69Y6OKg"),
  
   author : process.env.PACK_AUTHER|| "Suhail-MD",
   packname: process.env.PACK_NAME || "♥️",
